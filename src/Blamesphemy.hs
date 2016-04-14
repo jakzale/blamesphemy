c0f12b7ca8b8332e3ec4b1fc7255072c460b0bcf
Jakub Z <jakzale@gmail.com>
2016-04-14 12:27:56 +0100

    WIP on Any

--- src/Blamesphemy.hs
+++ src/Blamesphemy.hs
@@ -1,4 +1,23 @@
+{-#  LANGUAGE TypeApplications, GADTs #-}
+
 module Blamesphemy where
 
-blame :: IO ()
-blame = putStrLn "Enter Gradually Typed Haskell!"
+import Type.Reflection
+-- import Data.Proxy
+
+aList = typeRep @[Int]
+aFunction = typeRep @(Int -> Char)
+
+-- My own implementation of Dynamic
+
+data Any where
+  MkAny :: TypeRep a -> a -> Any
+
+toAny :: Typeable a => a -> Any
+toAny a = MkAny typeRep a
+
+{-
+fromAny :: Typeable a => Any -> Maybe a
+fromAny (MkAny (ra :: TypeRep a) (x :: a))
+  = Just Refl <- ra `eqTypeRep` rep
+-}
