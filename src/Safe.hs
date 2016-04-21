e27db86716ccb9e75e49561e61486f28639c44b7
Jakub Z <jakzale@gmail.com>
2016-04-21 13:28:59 +0100

    It appears to work

--- src/Safe.hs
+++ src/Safe.hs
@@ -27,7 +27,10 @@ fromAny (Any r v)
 data Same
 data ToAny
 data FromAny
-data Fun
+
+data Fun a b
+
+-- Not sure if this bits needed
 data Squish
 data Grow
 
@@ -43,7 +46,11 @@ instance (Typeable a) => Safer a Any ToAny where
 instance (Typeable b) => Safer Any b FromAny where
   safer _ = fromAny
 
-instance (Safer c a p, Safer b d q) => Safer (a -> Maybe b) (c -> Maybe d) Fun where
+type family Swap a where
+  Swap ToAny   = FromAny
+  Swap FromAny = ToAny
+
+instance (Safer c a p, Safer b d q) => Safer (a -> Maybe b) (c -> Maybe d) (Fun p q) where
   safer _ f = pure g
     where g :: c -> Maybe d
           g x = do
@@ -51,14 +58,27 @@ instance (Safer c a p, Safer b d q) => Safer (a -> Maybe b) (c -> Maybe d) Fun w
             y  <- f x'
             safer @(b) @(d) @(q) undefined y
 
- {- Not sure if this bit is necessary 
-instance Safer (Any -> Maybe Any) Any Squish where
-  safer _ f = 
-  -}
+ -- This one is slighlty troublesome
+instance (Safer Any a FromAny, Safer b Any ToAny) => Safer (a -> Maybe b) Any Squish where
+  safer _ f = do
+    f' <- safer @(a -> Maybe b) @(Any -> Maybe Any) @(Fun FromAny ToAny) undefined f
+    safer @(Any -> Maybe Any) @(Any) @(ToAny) undefined f'
+
+instance (Safer a Any ToAny, Safer Any b FromAny) => Safer Any (a -> Maybe b) Grow where
+  safer _ f = do
+    f' <- safer @(Any) @(Any -> Maybe Any) @(FromAny) undefined f
+    safer @(Any -> Maybe Any) @(a -> Maybe b) @(Fun ToAny FromAny) undefined f'
 
 -- This bit seems to work
 example1 = safer @(Integer) @(Any) @(ToAny) undefined 2
 example2 = do
   x <- example1
   safer @(Any) @(Integer) @(FromAny) undefined x
+
+example3 = do
+  a <- safer @(Integer -> Maybe Integer) @(Any) @(Squish) undefined (pure . (+5))
+  b <- safer @(Any) @(Any -> Maybe Any) @(FromAny) undefined a
+  c <- safer @(Any -> Maybe Any) @(Integer -> Maybe Integer) @(Fun ToAny FromAny) undefined b
+  c 3
+
 
